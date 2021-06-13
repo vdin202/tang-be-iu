82ddbbbd588cfbdb57f0aaf5fb549b977bd5aa4d
vdin202 <85819110+vdin202@users.noreply.github.com>
2021-06-13 13:17:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu anh ngay từ cái nhìn đầu tiên?',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'hk thích à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu cũng phải yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Nào về anh ăn bé nhé công chúa.',
     btnAccept: 'dạ <3',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Hk, ai cho ăn mà ăn :3',
     messLink: 'https://www.facebook.com/profile.php?id=100011104998023' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
